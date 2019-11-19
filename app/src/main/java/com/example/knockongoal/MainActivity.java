cc5454263158828c4c374e9641e7e209e1e756de
shumvlesu <sashumihin@gmail.com>
2019-11-19 16:32:45 +0300

    2

--- app/src/main/java/com/example/knockongoal/MainActivity.java
+++ app/src/main/java/com/example/knockongoal/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.knockongoal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.MediaController;
 
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
+import static android.Manifest.permission.CALL_PHONE;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +30,26 @@ public class MainActivity extends AppCompatActivity  {
         final GifImageView gif = findViewById(R.id.mushroom);
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                gif.setRotation(gif.getRotation()+180);
+                gif.setRotation(gif.getRotation() + 180);
+
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:+79180106162"));
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(i);
+                } else {
+                    requestPermissions(new String[]{CALL_PHONE}, 1);
+                }
+               // if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+              //      Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+79180106162"));
+               //     startActivity(intent);
+               // }
+
+
+
+
             }
         };
         gif.setOnClickListener(onClickListener);
