1f28aabe93a4f3bfcbebba0922dcc3e963bbabf6
shumvlesu <sashumihin@gmail.com>
2019-11-19 15:57:30 +0300

    2

--- app/src/main/java/com/example/knockongoal/MainActivity.java
+++ app/src/main/java/com/example/knockongoal/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.knockongoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final GifImageView gif = findViewById(R.id.mushroom);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gif.setRotation(gif.getRotation()+180);
+            }
+        };
+        gif.setOnClickListener(onClickListener);
+
+
+
+
+
+
+
     }
 }
