05a4818e8034a3d7e98cbddfde0da669e5a88ef3
shumvlesu <sashumihin@gmail.com>
2019-11-20 13:46:57 +0300

    Ворота прописал

--- app/src/main/java/com/example/knockongoal/Widget.java
+++ app/src/main/java/com/example/knockongoal/Widget.java
@@ -1,32 +1,55 @@
 package com.example.knockongoal;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Widget extends AppWidgetProvider {
 
+    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-
+        //Создаем новый RemoteViews
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        String now = dateFormat.format(date);
 
-        remoteViews.setTextViewText(R.id.dateTextView, now);
+        //Подготавливаем Intent для Broadcast
+        Intent active = new Intent(context, Widget.class);
+        active.setAction(ACTION_WIDGET_RECEIVER);
+        active.putExtra("msg", "Hello Habrahabr");
+
+        //создаем наше событие
+        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 0, active, 0);
+
+        //регистрируем наше событие
+        remoteViews.setOnClickPendingIntent(R.id.button1, actionPendingIntent);
+
+        //обновляем виджет
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        //Ловим наш Broadcast, проверяем и выводим сообщение
+        final String action = intent.getAction();
+        if (ACTION_WIDGET_RECEIVER.equals(action)) {
+            String msg = "null";
+            try {
+                msg = intent.getStringExtra("msg");
+            } catch (NullPointerException e) {
+                Log.e("Error", "msg = null");
+            }
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }
         super.onReceive(context, intent);
     }
 
