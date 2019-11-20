7a45509b57a62e5c14d415d56bdaa34463e20188
shumvlesu <sashumihin@gmail.com>
2019-11-20 11:20:43 +0300

    Ворота прописал

--- app/src/main/java/com/example/knockongoal/Widget.java
+++ app/src/main/java/com/example/knockongoal/Widget.java
@@ -1,6 +1,33 @@
 package com.example.knockongoal;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Widget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        String now = dateFormat.format(date);
+
+        remoteViews.setTextViewText(R.id.dateTextView, now);
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
 }
